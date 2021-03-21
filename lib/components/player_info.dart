b560946ff049141bdb2e88f5abca369345161f5a
SJKim <ksjksj0122@gmail.com>
2021-03-21 17:59:46 +0900

    Show player name and score with dynamic font size

--- lib/components/player_info.dart
+++ lib/components/player_info.dart
@@ -1,5 +1,7 @@
 import 'package:bxg_bjj_scoreboard/controller.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class PlayerInfo extends StatelessWidget {
   final PlayerType playerType;
@@ -8,13 +10,42 @@ class PlayerInfo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Expanded(child: Text('a')),
-        Expanded(child: Text('b')),
-        Expanded(child: Text('c')),
-      ],
+    Controller c = Get.find();
+
+    double dynamicFontSizeCriterion = MediaQuery.of(context).size.height;
+    var textStyle = TextStyle(color: Colors.white);
+    Color bgColor = Colors.green;
+
+    if (playerType == PlayerType.red) {
+      bgColor = Colors.red;
+    }
+
+    return Container(
+      color: bgColor,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Spacer(),
+          Expanded(
+            flex: 1,
+            child: TextField(
+              style: textStyle
+                  .merge(TextStyle(fontSize: dynamicFontSizeCriterion * 0.035)),
+              textAlign: TextAlign.center,
+              decoration: InputDecoration.collapsed(hintText: 'Player Name'),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Text(
+              '${c.getPlayerScore(this.playerType)}',
+              style: textStyle
+                  .merge(TextStyle(fontSize: dynamicFontSizeCriterion * 0.1)),
+            ),
+          ),
+          Expanded(flex: 2, child: Text('c')),
+        ],
+      ),
     );
   }
 }
